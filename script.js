27bc11d64d80cc39bf4379d6324995b8d0a2293e
NbAnt23 <94783520+NbAnt23@users.noreply.github.com>
2023-12-03 00:17:42 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,30 +2,49 @@ document.addEventListener("DOMContentLoaded", function () {
     let dropdowns = document.querySelectorAll('.dropdown');
     dropdowns.forEach(function (dropdown) {
         dropdown.addEventListener('mouseover', function (event) {
-            event.preventDefault(); // Prevent the default link behavior
+            event.preventDefault();
             this.querySelector('.dropdown-content').style.display = 'block';
         });
     });
-});
 
+    // Smooth scrolling for anchor links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
 
-// Define the text to be typed
-const textToType = "Invient";
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
 
-// Get the h1 element
-const typingTextElement = document.getElementById("typing-text");
+    // Define the text to be typed
+    const textToType = "Invient";
 
-// Function to simulate typing animation
-function typeText(index) {
-    typingTextElement.textContent = textToType.slice(0, index);
+    // Get the h1 element
+    const typingTextElement = document.getElementById("typing-text");
 
-    if (index < textToType.length) {
-        setTimeout(function () {
-            typeText(index + 1);
-        }, 150); // Adjust the typing speed as needed
+    // Function to simulate typing animation
+    function typeText(index) {
+        typingTextElement.textContent = textToType.slice(0, index);
+
+        if (index < textToType.length) {
+            setTimeout(function () {
+                typeText(index + 1);
+            }, 150); // Adjust the typing speed as needed
+        }
     }
-}
 
-// Start typing animation
-typeText(0);
+    // Start typing animation
+    typeText(0);
 
+    document.addEventListener('DOMContentLoaded', function () {
+        // Get the header element
+        const header = document.querySelector('.header-image');
+
+        // Add the show-header class after a delay (adjust the delay as needed)
+        setTimeout(function () {
+            header.classList.add('show-header');
+        }, 500);
+    });
+});
